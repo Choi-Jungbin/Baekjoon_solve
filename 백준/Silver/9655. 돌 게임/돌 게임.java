ef960306f6ebaa9091017e526f251c5352d06163
JB <73532407+Choi-Jungbin@users.noreply.github.com>
2025-01-24 17:15:38 +0900

    [Silver V] Title: 돌 게임, Time: 92 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n == 1) {
+			System.out.println("SK");
+			return;
+		}
+		boolean[] turn = new boolean[n];
+		turn[1] = true;
+		for(int i = 3; i < n; i++) {
+			if(i%2 == 0) {
+				if(!turn[i-1] && !turn[i-3]) turn[i] = true;
+			}
+			else {
+				if(!turn[i-1] || !turn[i-3]) turn[i] = true;
+			}
+		}
+		
+		System.out.println(turn[n-1] ? "CY" : "SK");
+	}
+}
