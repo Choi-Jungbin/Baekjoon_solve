d197e8803f54bbdcc21b0d92ca418bb9f8eb321a
JB <73532407+Choi-Jungbin@users.noreply.github.com>
2025-02-21 15:09:50 +0900

    [D5] Title: 수제 버거 장인, Time: 180 ms, Memory: 58,188 KB -BaekjoonHub

---
+++ SWEA/D5/3421. 수제 버거 장인/수제 버거 장인.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int n;
+	static boolean[] visit;
+	static List<Set<Integer>> list;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int test = 1; test <= T; test++) {
+			String[] s = br.readLine().split(" ");
+			n = Integer.parseInt(s[0]);
+			int m = Integer.parseInt(s[1]);
+			list = new ArrayList<>(n);
+			for(int i = 0; i < n; i++) {
+				list.add(new HashSet<Integer>());
+			}
+			
+			for(int i = 0; i < m; i++) {
+				s = br.readLine().split(" ");
+				int a = Integer.parseInt(s[0]) - 1;
+				int b = Integer.parseInt(s[1]) - 1;
+				list.get(a).add(b);
+				list.get(b).add(a);
+			}
+			
+			visit = new boolean[n];
+			System.out.println("#" + test + " " + calc(0));
+		}
+	}
+	
+	static int calc(int p) {
+		if(p == n) return 1;
+		int result = 0;
+		boolean check = true;
+		for(int a : list.get(p)) {
+			if(visit[a]) {
+				check = false;
+				break;
+			}
+		}
+		if(check) {
+			visit[p] = true;
+			result += calc(p+1);
+			visit[p] = false;
+		}
+		result += calc(p+1);
+		
+		return result;
+	}
+}
