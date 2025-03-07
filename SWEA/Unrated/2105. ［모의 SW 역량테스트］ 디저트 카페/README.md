1a38f6ae30e5fa80f2ed4fdafdcabedca08232e9
JB <73532407+Choi-Jungbin@users.noreply.github.com>
2025-03-07 16:27:27 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 269 ms, Memory: 95,244 KB -BaekjoonHub

---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 디저트 카페 - 2105 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5VwAr6APYDFAWu) 
+
+### 성능 요약
+
+메모리: 95,244 KB, 시간: 269 ms, 코드길이: 1,673 Bytes
+
+### 제출 일자
+
+2025-03-07 16:27
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int n;
+	static int max;
+	static int[][] map;
+	static int[] start = new int[2];
+	static Set<Integer> set = new HashSet<>();
+	static int[][] dir = {{1,1},{1,-1},{-1,-1},{-1,1}};
+	
+	static boolean check(int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < n;
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int test = 1; test <= T; test++) {
+			n = Integer.parseInt(br.readLine());
+			map = new int[n][n];
+			set.clear();
+			for(int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < n; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			// 시작 위치 먼저 지정하기
+			max = -1;
+			for(int i = 0; i < n-2; i++) {
+				for(int j = 1; j < n-1; j++) {
+					start[0] = i;
+					start[1] = j;
+					move(i,j,0);
+				}
+			}
+			System.out.println("#" + test + " " + max);
+		}
+	}
+	
+	// 시계방향으로 탐색
+	static void move(int x, int y, int rot) {
+		// 돌다가 처음 위치로 오면 길이 갱신
+		if(rot == 3 && start[0] == x && start[1] == y) {
+			max = Math.max(max, set.size());
+			return;
+		}
+		// 같은 종류를 만나면 나가기
+		if(set.contains(map[x][y])) return;
+		set.add(map[x][y]);
+		int dx = x + dir[rot][0];
+		int dy = y + dir[rot][1];
+		// 벽에 부딪히지 않으면 직진
+		if(check(dx,dy)) move(dx,dy,rot);
+		// 꺾어도 보기
+		if(rot != 3) {
+			rot++;
+			dx = x + dir[rot][0];
+			dy = y + dir[rot][1];
+			if(check(dx,dy)) move(dx,dy,rot);
+		}
+		set.remove(map[x][y]);
+	}
+}
